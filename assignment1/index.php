c0a95c8179c8e004371f840937ed0205d19794f1
Barry Molina <bazzaboy@gmail.com>
2022-02-18 13:21:02 -0600

    initial commit

---
+++ assignment1/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Molina INFOST 440 Assignment 1</title>
+	<link rel="stylesheet" href="./styles/style.css">
+</head>
+
+<body class="background-antiquewhite">
+	<header>
+		<h1>Assignment 1</h1>
+		<h3>Barry Molina</h3>
+	</header>
+	<main>
+		<h2>Pages</h2>
+		<table>
+			<thead>
+				<tr>
+					<th>Page</th>
+					<th>Link</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>PHP Info</td>
+					<td><a href="./phpinfo.php">phpinfo.php</a></td>
+				</tr>
+				<tr>
+					<td>Escape</td>
+					<td><a href="./escape.php">escape.php</a></td>
+				</tr>
+				<tr>
+					<td>Quotes</td>
+					<td><a href="./quotes.php">quotes.php</a></td>
+				</tr>
+				<tr>
+					<td>Predefined</td>
+					<td><a href="./predefined.php">predefined.php</a></td>
+				</tr>
+				<tr>
+					<td>Math</td>
+					<td><a href="./math.php">math.php</a></td>
+				</tr>
+				<tr>
+					<td>Constant</td>
+					<td><a href="./constant.php">constant.php</a></td>
+				</tr>
+				<tr>
+					<td>Functions</td>
+					<td><a href="./functions.php">functions.php</a></td>
+				</tr>
+				<tr>
+					<td>Conditions</td>
+					<td><a href="./conditions.php">conditions.php</a></td>
+				</tr>
+				<tr>
+					<td>Form</td>
+					<td><a href="./form.php">form.php</a></td>
+				</tr>
+			</tbody>
+		</table>
+	</main>
+</body>
+
+</html>
