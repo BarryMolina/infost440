4ed681c0786aadfc4cb254db1f22c5a677f87ed6
Barry Molina <bazzaboy@gmail.com>
2022-03-06 12:06:27 -0600

    grab post data and set sticky input values

--- assignment2/index.php
+++ assignment2/index.php
@@ -11,133 +11,230 @@
 
 <body>
 	<div class="container-sm">
-		<form class="row gx-3 gy-4">
-			<div class="col-md-6">
-				<label for="first-name" class="form-label">First Name</label>
-				<input type="text" name="first-name" class="form-control" id="first-name">
-			</div>
-			<div class="col-md-6">
-				<label for="last-name" class="form-label">Last Name</label>
-				<input type="text" name="last-name" class="form-control" id="last-name">
-			</div>
-			<div class="col-md-12">
-				<label for="address" class="form-label">Address</label>
-				<input type="text" name="address" class="form-control" id="address">
-			</div>
-			<div class="col-md-1">
-				Sex:
-			</div>
-			<div class="col-md-5">
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="radio" name="sex" id="sex-male" value="option1">
-					<label class="form-check-label" for="sex-male">Male</label>
-				</div>
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="radio" name="sex" id="sex-female" value="option2">
-					<label class="form-check-label" for="sex-female">Female</label>
-				</div>
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="radio" name="sex" id="sex-other" value="option2">
-					<label class="form-check-label" for="sex-other">Other</label>
-				</div>
-			</div>
-			<div class="col-md-1">
-				DOB:
-			</div>
-			<div class="col-md-5">
-				<div class="row gx-1">
-					<div class="col">
-						<select class="form-select form-select-sm" aria-label=".form-select-sm example">
-							<option selected>Day</option>
-							<option value="1">One</option>
-							<option value="2">Two</option>
-							<option value="3">Three</option>
-						</select>
-					</div>
-					<div class="col">
-						<select class="form-select form-select-sm" aria-label=".form-select-sm example">
-							<option selected>Month</option>
-							<option value="1">One</option>
-							<option value="2">Two</option>
-							<option value="3">Three</option>
-						</select>
-					</div>
-					<div class="col">
-						<select class="form-select form-select-sm" aria-label=".form-select-sm example">
-							<option selected>Year</option>
-							<option value="1">One</option>
-							<option value="2">Two</option>
-							<option value="3">Three</option>
-						</select>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<label for="height" class="form-label col">Height</label>
-				<input type="text" name="height" class="form-control col" id="height">
-			</div>
-			<div class="col-md-3">
-				<label for="weight" class="form-label">Weight</label>
-				<input type="text" name="weight" class="form-control" id="weight">
-			</div>
-			<div class="col-md-3">
-				<label for="eye-color" class="form-label">Eye Color</label>
-				<input type="text" name="eye-color" class="form-control" id="eye-color">
-			</div>
-			<div class="col-md-3">
-				<label for="hair-color" class="form-label">Hair Color</label>
-				<input type="text" name="hair-color" class="form-control" id="hair-color">
-			</div>
-			<div class="col-md-1">
-				Class:
-			</div>
-			<div class="col-md-5">
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="checkbox" name="class-A" id="class-A" value="option1">
-					<label class="form-check-label" for="class-A">A</label>
-				</div>
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="checkbox" name="class-B" id="class-B" value="option1">
-					<label class="form-check-label" for="class-B">B</label>
-				</div>
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="checkbox" name="class-C" id="class-C" value="option1">
-					<label class="form-check-label" for="class-C">C</label>
-				</div>
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="checkbox" name="class-D" id="class-D" value="option1">
-					<label class="form-check-label" for="class-D">D</label>
-				</div>
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="checkbox" name="class-M" id="class-M" value="option1">
-					<label class="form-check-label" for="class-M">M</label>
-				</div>
-			</div>
-			<div class="col-md-1">
-				Donor:
-			</div>
-			<div class="col-md-5">
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="radio" name="donor" id="donor-yes" value="yes">
-					<label class="form-check-label" for="donor-yes">Yes</label>
-				</div>
-				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="radio" name="donor" id="donor-no" value="no">
-					<label class="form-check-label" for="donor-no">No</label>
-				</div>
-			</div>
-			<div class="col-md-12">
-				<label class="form-label" for="instructions">Processing Instructions</label>
-				<textarea class="form-control" id="instructions" name="instructions" rows="5">Make it fast!</textarea>
-			</div>
-			<div class="col-12">
-				<button type="submit" class="btn btn-primary">Submit</button>
+		<form action="index.php" method="POST">
+			<?php
+			$fields = array(
+				'first-name' => array('value' => '', 'friendly-name' => 'first name'),
+				'last-name' => array('value' => '', 'friendly-name' => 'last name'),
+				'address' => array('value' => '', 'friendly-name' => 'address'),
+				'sex' => array('value' => '', 'friendly-name' => 'sex'),
+				'dob-day' => array('value' => 'Day', 'friendly-name' => 'birthday day'),
+				'dob-month' => array('value' => 'Month', 'friendly-name' => 'birthday month'),
+				'dob-year' => array('value' => 'Year', 'friendly-name' => 'birthday year'),
+				'height' => array('value' => '', 'friendly-name' => 'height'),
+				'weight' => array('value' => '', 'friendly-name' => 'weight'),
+				'eye-color' => array('value' => '', 'friendly-name' => 'eye color'),
+				'hair-color' => array('value' => '', 'friendly-name' => 'hair color'),
+				'donor' => array('value' => '', 'friendly-name' => 'donor preference'),
+				'instructions' => array('value' => '', 'friendly-name' => 'processing instructions'),
+			);
 
-			</div>
+			$class_fields = array(
+				'class-A' => false,
+				'class-B' => false,
+				'class-C' => false,
+				'class-D' => false,
+				'class-M' => false,
+			);
+
+			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+				// Populate variable values from POST data if it exists
+				// Else, display error message
+				foreach ($fields as $field => $attrs) {
+					// Check that the field is set and is different from the default value
+					if (isset($_POST[$field]) && $_POST[$field] != $attrs['value']) {
+						$fields[$field]['value'] = $_POST[$field];
+					} else {
+						echo "<div class='alert alert-danger' role='alert'>";
+						echo "Please enter your " . $attrs['friendly-name'];
+						echo "</div>";
+					}
+				}
+
+				// Initialize license class fields. Ensure at least one class is selected
+				$at_least_one = false;
+
+				foreach ($class_fields as $field => $default_value) {
+					if (isset($_POST[$field])) {
+						$class_fields[$field] = true;
+						$at_least_one = true;
+					}
+				}
+				if (!$at_least_one) {
+					echo "<div class='alert alert-danger' role='alert'>";
+					echo "You must select at least one license class";
+					echo "</div>";
+				}
+			}
+
+			// Initialize variables with values to be used in form
+			$first_name = $fields['first-name']['value'];
+			$last_name = $fields['last-name']['value'];
+			$address = $fields['address']['value'];
+			$sex = $fields['sex']['value'];
+			$dob_day = $fields['dob-day']['value'];
+			$dob_month = $fields['dob-month']['value'];
+			$dob_year = $fields['dob-year']['value'];
+			$height = $fields['height']['value'];
+			$weight = $fields['weight']['value'];
+			$eye_color = $fields['eye-color']['value'];
+			$hair_color = $fields['hair-color']['value'];
+			$donor = $fields['donor']['value'];
+			$instructions = $fields['instructions']['value'];
+
+			// License classes will have value of true or false
+			$class_A = $class_fields['class-A'];
+			$class_B = $class_fields['class-B'];
+			$class_C = $class_fields['class-C'];
+			$class_D = $class_fields['class-D'];
+			$class_M = $class_fields['class-M'];
+
+			// Arrays for DOB
+			$days_array = array_merge(array('Day'), range(1, 31));
+			$months_array = array('Month', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+			$years_array = array_merge(array('Year'), range(2022, 1900, -1));
+			?>
+			<!-- First name, last name, address -->
+			<fieldset class="row gx-3 gy-4">
+				<legend>Drivers License Application Form</legend>
+				<div class="col-md-6">
+					<label for="first-name" class="form-label">First Name</label>
+					<input type="text" name="first-name" class="form-control" id="first-name" value="<?php echo $first_name ?>">
+				</div>
+				<div class="col-md-6">
+					<label for="last-name" class="form-label">Last Name</label>
+					<input type="text" name="last-name" class="form-control" id="last-name" value="<?php echo $last_name ?>">
+				</div>
+				<div class="col-md-12">
+					<label for="address" class="form-label">Address</label>
+					<input type="text" name="address" class="form-control" id="address" value="<?php echo $address ?>">
+				</div>
+				<!-- Demographics -->
+				<div class="col-md-1">
+					Sex:
+				</div>
+				<div class="col-md-5">
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="radio" name="sex" id="sex-male" value="male" <?php echo ($sex == 'male' ? 'checked' : '') ?>>
+						<label class="form-check-label" for="sex-male">Male</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="radio" name="sex" id="sex-female" value="female" <?php echo ($sex == 'female' ? 'checked' : '') ?>>
+						<label class="form-check-label" for="sex-female">Female</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="radio" name="sex" id="sex-other" value="other" <?php echo ($sex == 'other' ? 'checked' : '') ?>>
+						<label class="form-check-label" for="sex-other">Other</label>
+					</div>
+				</div>
+				<div class="col-md-1">
+					DOB:
+				</div>
+				<!-- Date dropdowns -->
+				<div class="col-md-5">
+					<div class="row gx-1">
+						<div class="col">
+							<select class="form-select form-select-sm" name="dob-day">
+								<?php
+								foreach ($days_array as $day) {
+									$selected = $dob_day == $day ? 'selected' : '';
+									echo "<option value='$day' $selected>$day</option>";
+								}
+								?>
+							</select>
+						</div>
+						<div class="col">
+							<select class="form-select form-select-sm" name="dob-month">
+								<?php
+								foreach ($months_array as $month) {
+									$selected = $dob_month == $month ? 'selected' : '';
+									echo "<option value='$month' $selected>$month</option>";
+								}
+								?>
+							</select>
+						</div>
+						<div class="col">
+							<select class="form-select form-select-sm" name="dob-year">
+								<?php
+								foreach ($years_array as $year) {
+									$selected = $dob_year == $year ? 'selected' : '';
+									echo "<option value='$year' $selected>$year</option>";
+								}
+								?>
+							</select>
+						</div>
+					</div>
+				</div>
+				<!-- Physical appearance -->
+				<div class="col-md-3">
+					<label for="height" class="form-label col">Height</label>
+					<input type="text" name="height" class="form-control col" id="height" value="<?php echo $height ?>">
+				</div>
+				<div class="col-md-3">
+					<label for="weight" class="form-label">Weight</label>
+					<input type="text" name="weight" class="form-control" id="weight" value="<?php echo $weight ?>">
+				</div>
+				<div class="col-md-3">
+					<label for="eye-color" class="form-label">Eye Color</label>
+					<input type="text" name="eye-color" class="form-control" id="eye-color" value="<?php echo $eye_color ?>">
+				</div>
+				<div class="col-md-3">
+					<label for="hair-color" class="form-label">Hair Color</label>
+					<input type="text" name="hair-color" class="form-control" id="hair-color" value="<?php echo $hair_color ?>">
+				</div>
+				<div class="col-md-1">
+					Class:
+				</div>
+				<!-- Checkboxes for license class -->
+				<div class="col-md-5">
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="checkbox" name="class-A" id="class-A" value="true" <?php echo ($class_A ? 'checked' : '') ?>>
+						<label class="form-check-label" for="class-A">A</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="checkbox" name="class-B" id="class-B" value="true" <?php echo ($class_B ? 'checked' : '') ?>>
+						<label class="form-check-label" for="class-B">B</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="checkbox" name="class-C" id="class-C" value="true" <?php echo ($class_C ? 'checked' : '') ?>>
+						<label class="form-check-label" for="class-C">C</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="checkbox" name="class-D" id="class-D" value="true" <?php echo ($class_D ? 'checked' : '') ?>>
+						<label class="form-check-label" for="class-D">D</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="checkbox" name="class-M" id="class-M" value="true" <?php echo ($class_M ? 'checked' : '') ?>>
+						<label class="form-check-label" for="class-M">M</label>
+					</div>
+				</div>
+				<div class="col-md-1">
+					Donor:
+				</div>
+				<!-- Donor radiobuttons -->
+				<div class="col-md-5">
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="radio" name="donor" id="donor-yes" value="yes" <?php echo ($donor == 'yes' ? 'checked' : '') ?>>
+						<label class="form-check-label" for="donor-yes">Yes</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="radio" name="donor" id="donor-no" value="no" <?php echo ($donor == 'no' ? 'checked' : '') ?>>
+						<label class="form-check-label" for="donor-no">No</label>
+					</div>
+				</div>
+				<!-- Comment box -->
+				<div class="col-md-12">
+					<label class="form-label" for="instructions">Processing Instructions</label>
+					<textarea class="form-control" id="instructions" name="instructions" rows="5"><?php echo $instructions ?></textarea>
+				</div>
+				<!-- Submit button -->
+				<div class="col-12">
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+			</fieldset>
 		</form>
 	</div>
-
-
 </body>
 
 </html>
